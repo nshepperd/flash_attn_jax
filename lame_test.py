7c1dd7252bb3ae0dcd1e4c4d8c84ca034ead408b
nshepperd <nshepperd@gmail.com>
2024-02-18 21:40:13 +1100

    Start experimenting with sharding.

--- lame_test.py
+++ lame_test.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
         att = jnp.einsum('nlhd,nLhd->nhlL',q,k)
         att = jax.nn.softmax(att*softmax_scale, axis=-1)
         o = jnp.einsum('nhlL,nLhd->nlhd',att,v)
-        return o
+        return o.astype(q.dtype)
 
     # n l h d
 
@@ -43,74 +43,74 @@ if __name__ == '__main__':
         std = jnp.std(tensor)
         return f'[{shape}: {mn:.3g} | {mean:.3g}±{std:.3g} | {mx:.3g}]'
 
-    # q = jax.random.normal(jax.random.PRNGKey(0), [2, 4096, 4, 32]).astype(jnp.float16)
-    # k = jax.random.normal(jax.random.PRNGKey(1), [2, 4096, 4, 32]).astype(jnp.float16)
-    # v = jax.random.normal(jax.random.PRNGKey(2), [2, 4096, 4, 32]).astype(jnp.float16)
-
-    # @jax.jit
-    # def fwd(q,k,v):
-    #     return flash_mha(q,k,v)
-
-    # from jax.sharding import PositionalSharding
-    # from einops import rearrange
-
-    # sharding = PositionalSharding([*jax.devices(), *jax.devices(backend='cpu')])
-
-    # q = jax.device_put(q, sharding.reshape(2,1,1,1))
-    # k = jax.device_put(k, sharding.reshape(2,1,1,1))
-    # v = jax.device_put(v, sharding.reshape(2,1,1,1))
-    # jax.debug.visualize_array_sharding(rearrange(q, 'n l h d -> n (l h d)'))
-    # print(fwd.lower(q,k,v).compile().as_text())
-    # # exit()
-
-    print('==== forward ====')
-    q = jax.random.normal(jax.random.PRNGKey(0), [32, 4096, 4, 32]).astype(jnp.float16)
-    k = jax.random.normal(jax.random.PRNGKey(1), [32, 4096, 4, 32]).astype(jnp.float16)
-    v = jax.random.normal(jax.random.PRNGKey(2), [32, 4096, 4, 32]).astype(jnp.float16)
+    q = jax.random.normal(jax.random.PRNGKey(0), [2, 4096, 4, 32]).astype(jnp.float16)
+    k = jax.random.normal(jax.random.PRNGKey(1), [2, 4096, 4, 32]).astype(jnp.float16)
+    v = jax.random.normal(jax.random.PRNGKey(2), [2, 4096, 4, 32]).astype(jnp.float16)
 
     @jax.jit
     def fwd(q,k,v):
-        o = flash_mha(q,k,v)
-        for _ in range(32):
-            o = flash_mha(q,k,o)
-        return o
+        return flash_mha(q,k,v)
 
-    @jax.jit
-    def fwd_jax(q,k,v):
-        ro = pure_mha(q,k,v)
-        for _ in range(32):
-            ro = pure_mha(q,k,ro)
-        return ro
+    from jax.sharding import PositionalSharding
+    from einops import rearrange
 
-    o = fwd(q,k,v) #, softmax_scale=float(np.sqrt(1/32)))[0]
-    start = time.time()
-    o = fwd(q,k,v) #, softmax_scale=float(np.sqrt(1/32)))[0]
-    print('flash:', time.time() - start, 'seconds')
-    ro = fwd_jax(q,k,v)
-    start = time.time()
-    ro = fwd_jax(q,k,v)
-    print('jax:', time.time() - start, 'seconds')
-    print(pretty(jnp.abs(o - ro)), jnp.mean(jnp.abs(ro)))
+    sharding = PositionalSharding(jax.devices())
 
-    @jax.jit
-    @jax.grad
-    def grad_pure(inputs):
-        q,k,v = inputs
-        return pure_mha(q,k,v).sum()
+    q = jax.device_put(q, sharding.reshape(2,1,1,1))
+    k = jax.device_put(k, sharding.reshape(2,1,1,1))
+    v = jax.device_put(v, sharding.reshape(2,1,1,1))
+    jax.debug.visualize_array_sharding(rearrange(q, 'n l h d -> n (l h d)'))
+    print(fwd.lower(q,k,v).compile().as_text())
+    exit()
 
-    @jax.jit
-    @jax.grad
-    def grad_flash(inputs):
-        q,k,v = inputs
-        return flash_mha(q,k,v).sum()
+    # print('==== forward ====')
+    # q = jax.random.normal(jax.random.PRNGKey(0), [32, 4096, 4, 32]).astype(jnp.float16)
+    # k = jax.random.normal(jax.random.PRNGKey(1), [32, 4096, 4, 32]).astype(jnp.float16)
+    # v = jax.random.normal(jax.random.PRNGKey(2), [32, 4096, 4, 32]).astype(jnp.float16)
 
-    print('==== backward ====')
-    q = jax.random.normal(jax.random.PRNGKey(0), [1, 4, 2, 32]).astype(jnp.float16)
-    k = jax.random.normal(jax.random.PRNGKey(1), [1, 4, 2, 32]).astype(jnp.float16)
-    v = jax.random.normal(jax.random.PRNGKey(2), [1, 4, 2, 32]).astype(jnp.float16)
-    dq, dk, dv = grad_flash((q,k,v))
-    rdq, rdk, rdv = grad_pure((q,k,v))
-    # print(rdq, jnp.mean(jnp.abs(rdq)))
-    print('q', pretty(jnp.abs(dq - rdq)), jnp.mean(jnp.abs(rdq)))
-    print('k', pretty(jnp.abs(dk - rdk)), jnp.mean(jnp.abs(rdk)))
-    print('v', pretty(jnp.abs(dv - rdv)), jnp.mean(jnp.abs(rdv)))
+    # @jax.jit
+    # def fwd(q,k,v):
+    #     o = flash_mha(q,k,v)
+    #     for _ in range(32):
+    #         o = flash_mha(q,k,o)
+    #     return o
+
+    # @jax.jit
+    # def fwd_jax(q,k,v):
+    #     ro = pure_mha(q,k,v)
+    #     for _ in range(32):
+    #         ro = pure_mha(q,k,ro)
+    #     return ro
+
+    # o = fwd(q,k,v) #, softmax_scale=float(np.sqrt(1/32)))[0]
+    # start = time.time()
+    # o = fwd(q,k,v) #, softmax_scale=float(np.sqrt(1/32)))[0]
+    # print('flash:', time.time() - start, 'seconds')
+    # ro = fwd_jax(q,k,v)
+    # start = time.time()
+    # ro = fwd_jax(q,k,v)
+    # print('jax:', time.time() - start, 'seconds')
+    # print(pretty(jnp.abs(o - ro)), jnp.mean(jnp.abs(ro)))
+
+    # @jax.jit
+    # @jax.grad
+    # def grad_pure(inputs):
+    #     q,k,v = inputs
+    #     return pure_mha(q,k,v).sum()
+
+    # @jax.jit
+    # @jax.grad
+    # def grad_flash(inputs):
+    #     q,k,v = inputs
+    #     return flash_mha(q,k,v).sum()
+
+    # print('==== backward ====')
+    # q = jax.random.normal(jax.random.PRNGKey(0), [1, 4, 2, 32]).astype(jnp.float16)
+    # k = jax.random.normal(jax.random.PRNGKey(1), [1, 4, 2, 32]).astype(jnp.float16)
+    # v = jax.random.normal(jax.random.PRNGKey(2), [1, 4, 2, 32]).astype(jnp.float16)
+    # dq, dk, dv = grad_flash((q,k,v))
+    # rdq, rdk, rdv = grad_pure((q,k,v))
+    # # print(rdq, jnp.mean(jnp.abs(rdq)))
+    # print('q', pretty(jnp.abs(dq - rdq)), jnp.mean(jnp.abs(rdq)))
+    # print('k', pretty(jnp.abs(dk - rdk)), jnp.mean(jnp.abs(rdk)))
+    # print('v', pretty(jnp.abs(dv - rdv)), jnp.mean(jnp.abs(rdv)))
